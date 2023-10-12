920913c4a74949533ab4a151fe5d350af74dc330
Pedro Souza <pedrops244@hotmail.com>
2023-10-12 10:52:51 -0300

    separating components elements

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
    <v-app>
-      <v-app-bar flat class="border-b">
-         <v-app-bar-title>Meu app</v-app-bar-title>
-      </v-app-bar>
-
+      <Header />
       <v-main>
          <v-container>
             <h1>Dashboard</h1>
-            <v-row>
-               <v-col cols="12" md="4" lg="3">1</v-col>
-               <v-col cols="12" md="4" lg="3">1</v-col>
-               <v-col cols="12" md="4" lg="3">1</v-col>
-            </v-row>
+            <Card />
          </v-container>
       </v-main>
    </v-app>
@@ -19,4 +12,6 @@
 
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue';
+import Card from '@/components/Card.vue';
+import Header from '@/components/Header.vue';
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+   <v-app-bar flat class="border-b">
+      <v-app-bar-title>Meu app</v-app-bar-title>
+   </v-app-bar>
+</template>
+
+<script setup></script>
