86df5c9714ed7a796fc24e9c97e4d2bea48584b2
Pedro Souza <pedrops244@hotmail.com>
2023-10-11 21:20:43 -0300

    examples for utility class

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,20 @@
       <v-app-bar flat class="border-b">
          <v-app-bar-title>Meu app</v-app-bar-title>
       </v-app-bar>
+
       <v-main>
+         <v-container>
+            <div class="ma-10 d-flex justify-space-between">
+               <div style="width: 100px; height: 100px" class="pa-4 border rounded-lg elevation-2">Teste</div>
+               <div style="width: 100px; height: 100px" class="pa-4 border rounded-lg elevation-2">Teste</div>
+               <div style="width: 100px; height: 100px" class="pa-4 border rounded-lg elevation-2">Teste</div>
+            </div>
+         </v-container>
          <HelloWorld />
       </v-main>
    </v-app>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
