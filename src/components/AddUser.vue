d3b30ef5b3f86d50764a5e5fd61d42eb01e21148
Pedro Souza <pedrops244@hotmail.com>
2023-10-13 19:46:12 -0300

    add validation of addUser modal

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -8,17 +8,17 @@
         <v-card-text>
           <v-row>
             <v-col>
-              <v-text-field label="Nome" variant="outlined" clearable></v-text-field>
+              <v-text-field label="Nome" variant="outlined" clearable :rules="nameRules"></v-text-field>
             </v-col>
             <v-col>
-              <v-text-field label="Email" variant="outlined" clearable></v-text-field>
+              <v-text-field label="Email" variant="outlined" clearable :rules="emailRules"></v-text-field>
             </v-col>
           </v-row>
-          <v-select label="Cargo" variant="outlined" :items="['Admin', 'Gerente', 'Convidado']"></v-select>
+          <v-select class="mt-3" label="Cargo" variant="outlined" :items="['Admin', 'Gerente', 'Convidado']"></v-select>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn variant="elevated" color="success">Salvar</v-btn>
+          <v-btn class="mr-3" variant="elevated" color="success">Salvar</v-btn>
           <v-btn variant="tonal" color="error" @click="isDialogOpen = false">Cancelar</v-btn>
         </v-card-actions>
       </v-card>
@@ -29,4 +29,20 @@
 <script setup>
 import { ref } from 'vue';
 const isDialogOpen = ref(false);
+const emailRules = [
+  (value) => {
+    if (value) return true;
+    return 'O campo e-mail é obrigatório.';
+  },
+  (value) => {
+    if (value.includes('@')) return true;
+    return 'Insira um e-mail válido!';
+  },
+];
+const nameRules = [
+  (value) => {
+    if (value) return true;
+    return 'O campo nome é obrigatório.';
+  },
+];
 </script>
